05ab7cd9ba61d92c844f04c6dac9d3310d09d0e5
Nguyen Co <duongnguyenco0103@gmail.com>
2023-03-02 13:38:56 +0700

    config hosting

--- mykitchen_be/src/config/connDB.js
+++ mykitchen_be/src/config/connDB.js
@@ -1,10 +1,22 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("mykitchen_db", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+// const sequelize = new Sequelize("mykitchen_db", "root", null, {
+//   host: "localhost",
+//   dialect: "mysql",
+//   logging: false,
+// });
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 let connet = async () => {
   try {

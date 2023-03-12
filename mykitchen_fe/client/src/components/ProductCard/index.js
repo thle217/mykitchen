1d1f56f6239f6b9dbe94c9d3755df57b30a4b3b7
thle217 <118657050+thle217@users.noreply.github.com>
2023-03-13 01:29:51 +0700

    Update index.js

--- mykitchen_fe/client/src/components/ProductCard/index.js
+++ mykitchen_fe/client/src/components/ProductCard/index.js
@@ -8,54 +8,81 @@ import { VND } from "../../utils/currency";
 import cartAPI from "../../services/cartAPI";
 
 function ProductCard(props) {
-    //XỬ LÝ LƯU SẢN PHẨM VỪA CHỌN VÀO GIỎ HÀNG
-    const handleAddToCart = async () => {
-        let obj = {
-            ...props.product,
-            user_id: 1,
-            quantity: 1,
-            price: props.product.price,
-        };
 
-        await cartAPI.add(obj).then((res) => {
-            if (res.status === 200 || res.status === 201) {
-                successDialog();
-            } else if (res.status === 202) {
-                failDialog();
+
+    const user = useSelector(state => state.user);
+    const navigate = useNavigate();
+
+
+    //DIALOG THÔNG BÁO ĐĂNG NHẬP TRƯỚC KHI THÊM VÀO GIỎ
+    const loginDialog = () => {
+        Swal.fire({
+            title: "Bạn cần đăng nhập",
+            confirmButtonText: "Đăng nhập",
+            showCancelButton: true,
+            cancelButtonText: "Hủy",
+            customClass: {
+                title: "fs-5 text-dark",
+                confirmButton: "bg-primary shadow-none",
+                cancelButton: "bg-secondary shadow-none text-dark",
+            },
+        })
+        .then( async (result) => {
+            if (result.isConfirmed) {
+                navigate("/login");
             }
         });
     };
+
+
+    //XỬ LÝ LƯU SẢN PHẨM VỪA CHỌN VÀO GIỎ HÀNG
+    const handleAddToCart = async () => {
+        if(user.user_id) {
+            let obj = {
+                ...props.product,
+                user_id: user.user_id,
+                quantity: 1,
+                price: props.product.price
+            };
+    
+            await cartAPI.add(obj)
+            .then(res => {
+                if(res.status === 200 || res.status === 201) {
+                    successDialog();
+                }
+                else if(res.status === 202) {
+                    failDialog();
+                }
+            });
+        }
+        else {
+            loginDialog();
+        }
+    };
+
 
     return (
         <div className="pb-1 mb-5">
             <div className="card product-item border-0 mb-5 mt-5">
                 <div className="card-header product-img overflow-hidden bg-transparent border p-3">
                     <Link
-                        to="/product-details"
+                        to="/product-details" 
                         className="d-flex justify-content-center"
                         state={props.product}
                     >
-                        <img
-                            className="img-fluid w-75"
-                            src={props.product.url}
-                            alt=""
-                        />
+                        <img className="img-fluid w-75" src={props.product.url} alt=""/>
                     </Link>
                 </div>
                 <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
                     <Link
                         to="/product-details"
-                        style={{ textDecoration: "none" }}
+                        style={{textDecoration: 'none'}}
                         state={props.product}
                     >
-                        <h6 className="text-truncate mb-3 mx-2">
-                            {props.product.product_name}
-                        </h6>
+                        <h6 className="text-truncate mb-3 mx-2">{props.product.product_name}</h6>
                     </Link>
                     <div className="justify-content-center">
-                        <h6 className="text-danger">
-                            <b>{VND.format(props.product.price)}</b>
-                        </h6>
+                        <h6 className="text-danger"><b>{VND.format(props.product.price)}</b></h6>
                     </div>
                 </div>
                 <div className="card-footer d-flex justify-content-between bg-light border">
@@ -64,16 +91,12 @@ function ProductCard(props) {
                         className="btn btn-sm text-dark p-0"
                         state={props.product}
                     >
-                        <FontAwesomeIcon
-                            icon={faEye}
-                            className="text-primary mr-1"
-                        />
-                        Xem chi tiết
+                        <FontAwesomeIcon icon={faEye} className="text-primary mr-1"/>Xem chi tiết
                     </Link>
                     <button
                         type="button"
                         className="btn btn-outline-primary btn-add"
-                        style={{ backgroundColor: "transparent" }}
+                        style={{backgroundColor: 'transparent'}}
                         onClick={handleAddToCart}
                     >
                         Thêm vào giỏ
@@ -81,7 +104,7 @@ function ProductCard(props) {
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default ProductCard;
